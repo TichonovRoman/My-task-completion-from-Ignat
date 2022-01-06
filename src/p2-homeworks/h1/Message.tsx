adb7221b04d89737f891dffe822371fb5be26344
TichonovRoman <romikt@inbox.ru>
2022-01-06 18:55:42 +0300

    1 task completed

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import s from "./Message.module.css"
+
+
+type MessageType = {
+    avatar: any
+    name: string
+    message: string
+    time: string
+}
+
+
+let Message: React.FC<MessageType> = (props) => {
+    return (
+
+        <div className={s.message}>
+            <img className={s.avatar} alt={"user's avatar"} src={props.avatar}/>
+
+            <div id={s.triangle}></div>
+            <div className={s.contentBox}>
+                <div className={s.personalContent}>
+                    <div className={s.name}>{props.name}</div>
+                    <div className={s.messageText}>{props.message}</div>
+                </div>
+
+                <div className={s.time}>{props.time}</div>
+
+            </div>
+
+        </div>
+    )
+}
+
+
+export default Message
