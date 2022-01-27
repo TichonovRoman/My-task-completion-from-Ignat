acf9daf30082dd72208a6a203ed54a34f05a92cb
TichonovRoman <romikt@inbox.ru>
2022-01-27 14:03:40 +0300

    5.2: routes, navlink, menu

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,12 +1,27 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
+import {PATH} from "./Routes";
+import s from './menu.module.css'
+
+
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
+        <div  className={s.navlinks}>
+            <NavLink to={PATH.PRE_JUNIOR}>
+                PreJunior
+            </NavLink>
+
+            <NavLink to={PATH.JUNIOR}>
+                Junior
+            </NavLink>
+            <NavLink to={PATH.JUNIORPlus}>
+                Junior+
+            </NavLink>
 
         </div>
-    )
+
+         )
 }
 
 export default Header
--- src/p2-homeworks/h5/Routes.tsx
+++ src/p2-homeworks/h5/Routes.tsx
@@ -4,29 +4,34 @@ import {Route} from "react-router-dom";
 
 import PreJunior from "./pages/PreJunior";
 import Error404 from "./pages/Error404";
+import Junior from "./pages/Junior";
+import JuniorPlus from "./pages/JuniorPlus";
 
 
 export const PATH = {
     PRE_JUNIOR: '/pre-junior',
-    // add paths
+    JUNIOR: '/junior',
+    JUNIORPlus: '/junior+',
 }
 
 function Routes1() {
     return (
         <div>
             {/*Switch выбирает первый подходящий роут*/}
-            // add routes
+
             <Routes>
 
             {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
             {/*exact нужен чтоб указать полное совподение (что после '/' ничего не будет)*/}
             <Route path={'/'} element={() => <Navigate to={PATH.PRE_JUNIOR}/>}/>
 
-            <Route path={PATH.PRE_JUNIOR} element = {() => <PreJunior/>}/>
-                // add routes
+            <Route path={PATH.PRE_JUNIOR} element = {<PreJunior/>}/>
+            <Route path={PATH.JUNIOR} element = {<Junior/>}/>
+            <Route path={PATH.JUNIORPlus} element = {<JuniorPlus/>}/>
+
 
             {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
-            <Route element={() => <Error404/>}/>
+            <Route element={<Error404/>}/>
 
             </Routes>
         </div>
