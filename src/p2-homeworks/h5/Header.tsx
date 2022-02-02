aaade45c4bde79e4dee5d485f1118d448b55dae1
TichonovRoman <romikt@inbox.ru>
2022-02-02 11:55:17 +0300

    5: finished

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -4,24 +4,18 @@ import {PATH} from "./Routes";
 import s from './menu.module.css'
 
 
-
 function Header() {
     return (
-        <div  className={s.navlinks}>
-            <NavLink to={PATH.PRE_JUNIOR}>
-                PreJunior
-            </NavLink>
+        <div className={s.navlinks}>
 
-            <NavLink to={PATH.JUNIOR}>
-                Junior
-            </NavLink>
-            <NavLink to={PATH.JUNIORPlus}>
-                Junior+
-            </NavLink>
+            <NavLink to={PATH.PRE_JUNIOR}>PreJunior</NavLink>
+            <NavLink to={PATH.JUNIOR}>Junior</NavLink>
+            <NavLink to={PATH.JUNIORPlus}>Junior+</NavLink>
+            <img src={'https://blog.4d.com/wp-content/uploads/2017/08/UX2.png'} alt={"Наведи на меня"}/>
 
         </div>
 
-         )
+    )
 }
 
 export default Header
