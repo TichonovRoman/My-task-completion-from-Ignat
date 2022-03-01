c51c2bb3376f1d8e9a88ee9798ba91dbdc342254
TichonovRoman <romikt@inbox.ru>
2022-03-01 21:20:58 +0300

    task № 9 done

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -3,47 +3,62 @@ import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<Date>(new Date())
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+        clearTimeout(timerId)
     }
+
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
-            // setDate
+            setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
 
+
+
     const onMouseEnter = () => {
-        // show
+        setShow(true)
     }
     const onMouseLeave = () => {
-        // close
+        setShow(false)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const stringTime = date.toLocaleTimeString() // fix with date
+    const stringDate = date.toLocaleDateString()// fix with date
+    const style = {
+        display: "inline-block",
+        marginLeft: "5px",
+        width: "107px"
+    }
 
     return (
         <div>
-            <div
-                onMouseEnter={onMouseEnter}
-                onMouseLeave={onMouseLeave}
-            >
-                {stringTime}
-            </div>
+
+                <input
+                    onMouseEnter={onMouseEnter}
+                    onMouseLeave={onMouseLeave}
+                    className="form-control"
+                    type="text"
+                    placeholder="Readonly input here…"
+                    readOnly
+                    value={stringTime}
+                    style = {{width: "220px", fontSize: "50px", marginLeft: "5px"}}
+                />
+
+
 
             {show && (
-                <div>
+                <div style = {{width: "220px", fontSize: "50px", marginLeft: "5px"}}>
                     {stringDate}
                 </div>
             )}
 
-            <SuperButton onClick={start}>start</SuperButton>
-            <SuperButton onClick={stop}>stop</SuperButton>
+            <SuperButton style={style} onClick={start}>start</SuperButton>
+            <SuperButton style={style} onClick={stop}>stop</SuperButton>
 
         </div>
     )
